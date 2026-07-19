ab0c2c6b1db07d2b11e0156d28ffbc53aff504ee
atikaylna <atikayuliana12345@gmail.com>
2026-07-19 18:18:12 +0700

    daaaaa

--- script.js
+++ script.js
@@ -288,24 +288,7 @@ function typeWriter(){
 window.onload = typeWriter;
 
 const themeToggle = document.getElementById("theme-toggle");
-const body = document.body;
-const icon = themeToggle.querySelector("i");
 
-if(localStorage.getItem("theme") === "light"){
-    body.classList.add("light-mode");
-    icon.classList.replace("fa-moon","fa-sun");
-}
-
-themeToggle.addEventListener("click", () => {
-
-    body.classList.toggle("light-mode");
-
-    if(body.classList.contains("light-mode")){
-        icon.classList.replace("fa-moon","fa-sun");
-        localStorage.setItem("theme","light");
-    }else{
-        icon.classList.replace("fa-sun","fa-moon");
-        localStorage.setItem("theme","dark");
-    }
-
+themeToggle.addEventListener("click", function () {
+    document.body.classList.toggle("light-mode");
 });
