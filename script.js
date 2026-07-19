df9dd9ae3bb807ae4ef554279c8acb4cf390814c
atikaylna <atikayuliana12345@gmail.com>
2026-07-19 18:08:05 +0700

    maaaaaaaaa

--- script.js
+++ script.js
@@ -287,3 +287,20 @@ function typeWriter(){
 
 window.onload = typeWriter;
 
+const themeToggle = document.getElementById("theme-toggle");
+const body = document.body;
+const icon = themeToggle.querySelector("i");
+
+themeToggle.addEventListener("click", () => {
+
+    body.classList.toggle("light-mode");
+
+    if(body.classList.contains("light-mode")){
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
+    }else{
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
+    }
+
+});
